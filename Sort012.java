8a3e9eb7f78c148d6187da559753c2c2116fecc1
HetLukhi <24it043@charusat.edu.in>
2025-10-07 21:52:32 +0530

    Create Sort012.java

---
+++ Sort012.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Sort012 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for(int i=0; i<n; i++) nums[i] = sc.nextInt();
+        
+        int low = 0, mid = 0, high = n - 1;
+        while(mid <= high){
+            if(nums[mid] == 0){
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++; mid++;
+            } else if(nums[mid] == 1){
+                mid++;
+            } else {
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
+        }
+        
+        for(int i=0; i<n; i++) System.out.print(nums[i] + " ");
+    }
+}
